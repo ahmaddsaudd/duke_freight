32ae5d4bd40ef68544a68fa568b79dbf7d86289d
ahmaddsaudd <ahmedsaud1999@gmail.com>
2022-12-23 16:30:38 +0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,44 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">Duke Freight</span>{" "}
+            <span className="text-white">Logistics!</span>
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            ONE STOP<br className="sm:block hidden" />{" "}
+            <span className="text-gradient">SOLUTION</span>{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          To All Your Transportational Needs!
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of logistics experts enables our customers to have enjoy hassle free transportation,
+          so you can aim for success and we will get it to you!
+        </p>
+      </div>
+
+
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
